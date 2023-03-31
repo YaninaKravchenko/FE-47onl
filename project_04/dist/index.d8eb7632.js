67010dd133036bb9379b795c1bbd79a9d1312d09
YaninaKravchenko <yakrauchanka@gmail.com>
2023-03-31 13:32:03 +0300

    add project ACTIVEBOX

---
+++ project_04/dist/index.d8eb7632.js
@@ -0,0 +1,1 @@
+new Swiper(".swiper",{slidesPerView:1,spaceBetween:30,pagination:{el:".swiper-pagination",clickable:!0},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
---
+++ project_04/src/js/app.js
@@ -0,0 +1,24 @@
+/*const swiper = new Swiper('.swiper', {
+    speed: 400,
+    spaceBetween: 10,
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+  });*/
+
+
+  var swiper = new Swiper('.swiper', {
+    slidesPerView: 1,
+   /* slidesPerColumn: 2,*/
+    spaceBetween: 30,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+    }
+  });
